1ce22c8628e92e7f579e52ff3e1b2608ae582993
Beatriz Carvalho <beatriz.l.carvalho26399@gmail.com>
2021-11-25 00:46:45 +0000

    Update inicial

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import nltk
+import tensorflow as tf
+
+#carregar_ficheiro
+dados = pd.read_csv('dados.csv')
+dados.head()
+
+#limpeza_dados
+#remove_pontuacao_e_simbolos
+
+
+#converter_numeros_classes
